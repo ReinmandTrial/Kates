d53b459f6875d94aeeba1dc785849663ac4221eb
Elvis Rauza <elvis@datafix.lv>
2022-07-25 11:19:56 +0300

    Gitignore remove node_modules from git

--- src/js/partials/template-home.js
+++ src/js/partials/template-home.js
@@ -1,62 +1,61 @@
 // import Swiper bundle with all modules installed
 import Swiper from "swiper/bundle";
 
-// import styles bundle
-import "swiper/css/bundle";
-
 const TemplateHome = {
   init() {
-    console.log("Home view");
+    this.events();
+  },
+
+  events() {
+    // header burger
+    var burgerMenu = document.getElementById("burger");
+    var overlay = document.getElementById("menu");
+    burgerMenu.addEventListener("click", function () {
+      this.classList.toggle("close");
+      overlay.classList.toggle("overlay");
+    });
+
+    // homepage top Swiper slider
+    const SwiperMainSlider = new Swiper(".main-slider", {
+      // Optional parameters
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: ".main-slider-pagination",
+        clickable: true,
+      },
+    });
+
+    // products Swiper slider
+    const SwiperProductSlider = new Swiper(".product-slider", {
+      // Optional parameters
+      slidesPerView: "auto",
+      spaceBetween: 8,
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 16,
+        },
+        // when window width is >= 640px
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".product-slider-button-next",
+        prevEl: ".product-slider-button-prev",
+      },
+
+      // If we need pagination
+      pagination: {
+        el: ".product-slider-pagination",
+        clickable: true,
+      },
+    });
   },
 };
-
-// header burger
-var burgerMenu = document.getElementById('burger');
-var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
-
-// homepage top Swiper slider
-const SwiperMainSlider = new Swiper(".main-slider", {
-  // Optional parameters
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".main-slider-pagination",
-    clickable: true,
-  },
-});
-
-// products Swiper slider
-const SwiperProductSlider = new Swiper(".product-slider", {
-  // Optional parameters
-  slidesPerView: "auto",
-  spaceBetween: 8,
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 16,
-    },
-    // when window width is >= 640px
-    1200: {
-      slidesPerView: 4,
-    },
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".product-slider-button-next",
-    prevEl: ".product-slider-button-prev",
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".product-slider-pagination",
-    clickable: true,
-  },
-});
 
 export default TemplateHome;
