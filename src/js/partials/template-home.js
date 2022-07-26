6d8cc616b5f6c1fc23bc91d2ae62253a0185a4a5
ReinmandTrial <max.gluschtsov@gmail.com>
2022-07-26 15:50:42 +0300

    Responsive

--- src/js/partials/template-home.js
+++ src/js/partials/template-home.js
@@ -8,12 +8,50 @@ const TemplateHome = {
 
   events() {
     // header burger
-    var burgerMenu = document.getElementById("burger");
-    var overlay = document.getElementById("menu");
+    let burgerMenu = document.getElementById("burger");
+    let overlay = document.getElementById("menu");
     burgerMenu.addEventListener("click", function () {
       this.classList.toggle("close");
       overlay.classList.toggle("overlay");
+      document.body.classList.toggle("menu-opened");
     });
+
+    // private or company tabs in header
+    let privateTabSelector = document.getElementsByClassName("private-tab-selector")[0];
+    let companyTabSelector = document.getElementsByClassName("company-tab-selector")[0];
+    let privateTabContent = document.getElementsByClassName("private-tab-content")[0];
+    let companyTabContent = document.getElementsByClassName("company-tab-content")[0];
+    privateTabSelector.addEventListener("click", function () {
+      this.classList.add("active");
+      privateTabContent.classList.add("active");
+      companyTabSelector.classList.remove("active");
+      companyTabContent.classList.remove("active");
+    });
+    companyTabSelector.addEventListener("click", function () {
+      this.classList.add("active");
+      companyTabContent.classList.add("active");
+      privateTabSelector.classList.remove("active");
+      privateTabContent.classList.remove("active");
+    });
+
+    // making the mobile header, first checking the screen size
+    if (window.screen.width <= 900) {
+      let topHeaderTabs = document.getElementsByClassName("top-header-list-tabs")[0];
+      let topHeaderTabsContent = document.getElementsByClassName("main-header-links")[0];
+      let topHeaderLinks1 = document.querySelectorAll(".top-header-list-links")[0];
+      let topHeaderLinks2 = document.querySelectorAll(".top-header-list-links")[1];
+
+      // new containers
+      let topHeaderTabsMobile = document.getElementsByClassName("main-header-mobile-tabs")[0];
+      let topHeaderTabsMobileContent = document.getElementsByClassName("main-header-mobile-tabs-content")[0];
+      let topHeaderMobileLinks = document.getElementsByClassName("main-header-mobile-links")[0];
+
+      // moving the desktop elements to new containers
+      topHeaderTabsMobile.appendChild(topHeaderTabs);
+      topHeaderTabsMobileContent.appendChild(topHeaderTabsContent);
+      topHeaderMobileLinks.appendChild(topHeaderLinks1);
+      topHeaderMobileLinks.appendChild(topHeaderLinks2);
+    }
 
     // homepage top Swiper slider
     const SwiperMainSlider = new Swiper(".main-slider", {
@@ -37,7 +75,9 @@ const TemplateHome = {
           slidesPerView: 2,
           spaceBetween: 16,
         },
-        // when window width is >= 640px
+        800: {
+          slidesPerView: 3,
+        },
         1200: {
           slidesPerView: 4,
         },
