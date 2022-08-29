a8e0068c0c29b20cccee78fa3f6356cb0826b7d6
ReinmandTrial <max.gluschtsov@gmail.com>
2022-08-29 21:40:37 +0300

    any fixes

--- src/js/partials/template-material-shop.js
+++ src/js/partials/template-material-shop.js
@@ -4,6 +4,7 @@ const TemplateMaterialShop = {
   },
 
   events() {
+    // open comparison
     const comparisonOpener = document.querySelectorAll(".product-block-compare-link");
     comparisonOpener.forEach(function (el) {
       el.addEventListener("click", function (e) {
@@ -12,9 +13,21 @@ const TemplateMaterialShop = {
       });
     });
 
+    // close it
     document.querySelector('.comparison-chosen-close').addEventListener("click", function () {
       document.querySelector('.comparison-chosen').classList.remove("open");
     });
+
+    // remove element from comparison
+    let comparisonEl = document.querySelectorAll('.comparison-chosen-list-item img');
+    let button = document.querySelectorAll('.comparison-chosen-list-item-remove');
+    let remove = function () {
+      this.remove();
+      document.querySelector('.comparison-chosen-list-item img').remove();
+    };
+    for (var i = 0, len = comparisonEl.length; i < len; i++) {
+      button[i].addEventListener('click', remove, false);
+    }
   },
 };
 
